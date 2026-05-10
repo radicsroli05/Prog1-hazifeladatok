744338627c740dc84f1f1ac272f74757cdc2a11e
radicsroli05 <70635789+radicsroli05@users.noreply.github.com>
2026-05-10 12:40:56 +0200

    Create ezkis.c

---
+++ ezkis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int osszeg = 0;
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            osszeg += i;
+        }
+    }
+    printf("Azon 1000-nél kisebb számok összege, melyek 3-nak vagy 5-nek többszörösei: %d\n", osszeg);
+    
+    return 0;
+}
